2502cb0d54e767668d82a9d19081755b41c9e40f
Jeet Khondker <jeetzhkhondker@gmail.com>
2025-11-17 12:24:41 +0900

    feature (#28) : Created User Interface / Type

---
+++ frontend/src/types/user.ts
@@ -0,0 +1,10 @@
+export interface User {
+  id: string;
+  email: string;
+  firstName: string;
+  middleName: string;
+  lastName: string;
+  isActive: boolean;
+  createdAt: string;
+  updatedAt: string;
+}
